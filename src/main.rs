f113b8aeb13ada77f5db2720124017095f7251c7
Jadedboat780 <nikitatokin2006@gmail.com>
2023-09-13 16:13:11 +0300

    Реализовал минимальный VGA буффер

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[no_mangle]
+fn _start() ->! {
+    vga_buffer::print_something(b'H', "ello world");
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
