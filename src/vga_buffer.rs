13f234ec1a3500c341674d46e2d8f6208b1d9a80
Jadedboat780 <nikitatokin2006@gmail.com>
2023-09-20 11:04:14 +0300

    Чуть подправил код

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,9 +1,8 @@
 use core::fmt;
 use lazy_static::lazy_static;
-use spin::Mutex;
 use volatile::Volatile;
+use spin::Mutex;
 
-#[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
@@ -55,36 +54,7 @@ pub struct Writer {
     buffer: &'static mut Buffer,
 }
 
-lazy_static! {
-    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Pink, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    });
-}
-
 impl Writer {
-    pub fn write_byte(&mut self, byte: u8) {
-        match byte {
-            b'\n' => self.new_line(),
-            byte => {
-                if self.column_position >= BUFFER_WIDTH {
-                    self.new_line();
-                }
-
-                let row = BUFFER_HEIGHT - 1;
-                let col = self.column_position;
-
-                let color_code = self.color_code;
-                self.buffer.chars[row][col].write(ScreenChar {
-                    ascii_character: byte,
-                    color_code,
-                });
-                self.column_position += 1;
-            }
-        }
-    }
-
     fn new_line(&mut self) {
         for row in 1..BUFFER_HEIGHT {
             for col in 0..BUFFER_WIDTH {
@@ -106,6 +76,27 @@ impl Writer {
         }
     }
 
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
@@ -121,6 +112,14 @@ impl fmt::Write for Writer {
         self.write_string(s);
         Ok(())
     }
+}
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Green, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
 }
 
 #[macro_export]
@@ -140,26 +139,24 @@ pub fn _print(args: fmt::Arguments) {
     WRITER.lock().write_fmt(args).unwrap();
 }
 
-
-#[test_case]
-fn test_println_simple() {
-    println!("test_println_simple output");
-}
-
-#[test_case]
-fn test_println_many() {
-    for _ in 0..200 {
-        println!("test_println_many output");
-    }
-}
-
-
-#[test_case]
-fn test_println_output() {
-    let s = "Some tests string that fits on a single line";
-    println!("{}", s);
-    for (i, c) in s.chars().enumerate() {
-        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
-        assert_eq!(char::from(screen_char.ascii_character), c);
-    }
-}
+// #[test_case]
+// fn test_println_simple() {
+//     println!("test_println_simple output");
+// }
+//
+// #[test_case]
+// fn test_println_many() {
+//     for _ in 0..200 {
+//         println!("test_println_many output");
+//     }
+// }
+//
+// #[test_case]
+// fn test_println_output() {
+//     let s = "Some tests string that fits on a single line";
+//     println!("{}", s);
+//     for (i, c) in s.chars().enumerate() {
+//         let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+//         assert_eq!(char::from(screen_char.ascii_character), c);
+//     }
+// }
