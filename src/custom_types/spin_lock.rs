1c9b3dc9d3b75a9c7b652dab0d5245b601e905a5
nikit <nikitatokin2006@gmail.com>
2024-02-25 14:29:08 +0300

    улучшил документацию и добавил доп. тест

--- src/custom_types/spin_lock.rs
+++ src/custom_types/spin_lock.rs
@@ -1,48 +1,54 @@
-use core::cell::UnsafeCell;
 use core::sync::atomic::{
     AtomicBool,
     Ordering::{Acquire, Release},
 };
+use core::cell::UnsafeCell;
 use core::fmt;
 use core::ops::{Deref, DerefMut};
 
+#[derive(Default)]
 pub struct SpinLock<T> {
-    locked: AtomicBool,
-    value: UnsafeCell<T>, //предоставление изменяемости объекта
+    locked: AtomicBool, // состояние объекта (свободен или заблокирован)
+    value: UnsafeCell<T> // предоставление внутринней изменяемости объекта
 }
 
 impl<T> SpinLock<T> {
-    //создание экземпляра объекта
+    // конструктор
     pub const fn new(value: T) -> Self {
         Self {
             locked: AtomicBool::new(false),
-            value: UnsafeCell::new(value), //необходимо для внутренней изменивости
+            value: UnsafeCell::new(value)
         }
     }
 
-    //блокировка объекта
+    // блокировка объекта
     pub fn lock(&self) -> Guard<T> {
+        if self.is_locked(){
+            panic!("SpinLock: double lock")
+        }
         while self.locked.swap(true, Acquire) {
+            // подсказка циклу вращения (сообщает процессору, что мы вращаемся, ожидая каких-либо изменени)
+            // приводит к созданию специальной инструкции для оптимизации поведения блокировки
             core::hint::spin_loop();
         }
         Guard { lock: self }
     }
 
-    //разблокировка объекта(предназначена лишь для вызова диструктором)
+    // освобождение объекта (предназначена лишь для вызова диструктором)
     unsafe fn unlock(&self) {
         self.locked.store(false, Release);
     }
 
-    //проверяем, заблокирован ли объект
+    // проверяем, заблокирован ли объект
     pub fn is_locked(&self) -> bool {
         self.locked.load(Acquire)
     }
 }
 
-//указываем, что использование типа между потоками является безопасным
+// указываем, что использование типа между потоками является безопасным
 unsafe impl<T> Sync for SpinLock<T> where T: Send {}
 
-//реализация вывода в консоль для отладки
+// реализация вывода в консоль для отладки
 impl<T: fmt::Debug> fmt::Debug for SpinLock<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("SpinLock")
@@ -52,12 +58,12 @@ impl<T: fmt::Debug> fmt::Debug for SpinLock<T> {
     }
 }
 
-//безопасный интерфейс
-pub struct Guard<'a, T> {
-    lock: &'a SpinLock<T>,
+// безопасный интерфейс для блокировки
+pub struct Guard<'lock, T> {
+    lock: &'lock SpinLock<T>,
 }
 
-//реализация вывода в консоль для отладки
+// реализация вывода в консоль для отладки
 impl<T: fmt::Debug> fmt::Debug for Guard<'_, T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Guard")
@@ -66,7 +72,7 @@ impl<T: fmt::Debug> fmt::Debug for Guard<'_, T> {
     }
 }
 
-//реализация разыменования указателя
+// реализация разыменования
 impl<T> Deref for Guard<'_, T> {
     type Target = T;
     fn deref(&self) -> &T {
@@ -74,20 +80,21 @@ impl<T> Deref for Guard<'_, T> {
     }
 }
 
-//реализация разыменования указателя с получением изменяемой ссылки
+// реализация разыменования с получением изменяемой ссылки
 impl<T> DerefMut for Guard<'_, T> {
     fn deref_mut(&mut self) -> &mut T {
         unsafe { &mut *self.lock.value.get() }
     }
 }
 
-//реализация диструктора
+// реализация диструктора (особождение блокировки)
 impl<T> Drop for Guard<'_, T> {
     fn drop(&mut self) {
         unsafe { self.lock.unlock() }
     }
 }
 
+// тесты для проверки работы SpinLock
 #[cfg(test)]
 mod test {
     use super::*;
@@ -109,5 +116,11 @@ mod test {
 
         drop(lock_array);
         assert_eq!(array.is_locked(), false);
+    }
+
+    #[test_case]
+    fn test_defer(){
+        let num = SpinLock::new(12);
+        assert_eq!(12, *num.lock());
     }
 }
