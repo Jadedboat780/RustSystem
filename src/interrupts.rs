d683fe9da52c303c43928f73a4db4b7f3e9d8b0b
tokin_nikita <nikitatokin2006@gmail.com>
2025-06-28 19:06:56 +0300

    update interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,4 +1,11 @@
-use super::{custom_types::spin_lock::SpinLock, gdt, hlt_loop, println};
+use core::ops::IndexMut;
+use super::{gdt, hlt_loop};
+use crate::{
+    datetime::{CURRENT_TIME, TICKS},
+    println,
+};
+use core::sync::atomic::Ordering;
+use custom_types::spin_lock::SpinLock;
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
@@ -9,8 +16,6 @@ const PIC_2_OFFSET: u8 = 40;
 
 pub static PICS: SpinLock<ChainedPics> =
     SpinLock::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
-
-// функция-обработчик прерываний таймера:
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
@@ -21,18 +26,17 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
-        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
-        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        idt.index_mut(InterruptIndex::Timer.as_u8()).set_handler_fn(timer_interrupt_handler);
+        idt.index_mut(InterruptIndex::Keyboard.as_u8()).set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
 
-// тип для обработки прерываний таймера
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
-    Keyboard,
+    Keyboard = PIC_1_OFFSET + 1,
 }
 
 impl InterruptIndex {
@@ -49,12 +53,10 @@ pub fn init_idt() {
     IDT.load();
 }
 
-// функция-обработчик ошибок прерываний
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
-// функция-обработчик ошибок страниц
 extern "x86-interrupt" fn page_fault_handler(
     stack_frame: InterruptStackFrame,
     error_code: PageFaultErrorCode,
@@ -69,29 +71,29 @@ extern "x86-interrupt" fn page_fault_handler(
     hlt_loop();
 }
 
-// функция-обработчик двойных ошибок
-extern "x86-interrupt" fn double_fault_handler(
-    stack_frame: InterruptStackFrame,
-    _error_code: u64,
-) -> ! {
+extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
-// функция-обработчик прерываний таймера
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    TICKS.fetch_add(1, Ordering::Relaxed);
+
+    if TICKS.load(Ordering::Relaxed) % 1000 == 0 {
+        let mut time = CURRENT_TIME.lock();
+        time.update();
+    }
+
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
     }
 }
 
-// функция-обработчик прерываний клавиатуры
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
     use x86_64::instructions::port::Port;
 
     let mut port = Port::new(0x60);
-    let scancode: u8 = unsafe { port.read() };
-    crate::task::keyboard::add_scancode(scancode);
+    let _scancode: u8 = unsafe { port.read() };
 
     unsafe {
         PICS.lock()
